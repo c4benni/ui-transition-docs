5e1cd12cc6a47def5be69204e514fb1ea871a291
c4benni <c4benni@gmail.com>
2022-01-30 23:27:18 +0100

    create Loader.vue

---
+++ src/components/Loader.vue
@@ -0,0 +1,29 @@
+<template>
+  <IconWrapper tag="div" class="Loader">
+    <Spinner />
+  </IconWrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import IconWrapper from "./icons/IconWrapper.vue";
+import Spinner from "./icons/Spinner.vue";
+
+export default defineComponent({
+  name: "Loader",
+  components: { IconWrapper, Spinner },
+});
+</script>
+
+<style scoped>
+@keyframes spin-loader {
+  to {
+    transform: rotate(1turn) translateZ(0);
+  }
+}
+
+.Loader {
+  opacity: 0.7;
+  animation: spin-loader var(--duration, 0.75s) linear infinite;
+}
+</style>
