c57078bb78c2dd4c8e8603ab3a0e9951c6185977
c4benni <c4benni@gmail.com>
2022-01-30 22:55:08 +0100

    create intro.vue

--- src/components/sections/Main/Intro/index.vue
+++ src/components/sections/Main/Intro/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <section class="mt-6 sm:mt-12 grid md:grid-cols-[1fr,auto] md:gap-x-3 lg:gap-x-6 md:grid-rows-[148px] lg::grid-rows-[164px]">
+  <section class="mt-6 xs:mt-12 sm:mt-12 grid md:grid-cols-[1fr,400px] md:gap-x-3 lg:gap-x-6 md:grid-rows-[148px] lg::grid-rows-[164px]">
     <h2
       :class="[
-        'font-extrabold text-4xl sm:text-5xl leading-normal sm:leading-normal mb-3 col-start-1',
-        classNames.headline,
-        centerHeadlines
+        'font-extrabold text-3xl xs:text-4xl sm:text-5xl leading-normal xs:leading-normal sm:leading-normal mb-6 md:mb-3 col-start-1 justify-self-center md:justify-self-start',
       ]"
     >
       Create Dynamic <br />CSS Springs.
@@ -12,7 +10,7 @@
 
     <div
       :class="[
-        'text-base leading-relaxed sm:text-lg lg:text-base sm:leading-loose lg:leading-loose col-start-1',
+        'text-[0.95rem] leading-relaxed xs:text-base xs:leading-relaxed sm:text-lg sm:leading-loose lg:text-base lg:leading-loose col-start-1',
         classNames.paragraph,
         centerHeadlines,
         {
@@ -28,14 +26,14 @@
       <p>
         UiTransition provides an easy way to create
         <strong><em>CSS powered</em></strong> spring animations that can be
-        easily created, tweaked, and extended to create butter smooth
+        easily setup, tweaked, and extended to create butter smooth
         transitions between states.
       </p>
     </div>
 
-    <Button size="md" class="col-start-1" @click="theme.dark = !theme.dark"> Browse API </Button>
+    <Button size="md" class="col-start-1 mx-3 xs:mx-6 md:ml-0 md:mr-12 mt-12" @click="theme.dark = !theme.dark"> Browse API </Button>
 
-    <Mockup v-if="!breakpoint.isMobile" class="col-start-2 row-start-1"/>
+    <Mockup v-if="!breakpoint.isMobile" class="col-start-2 row-start-1 w-[312px] h-[calc(100vh-56px)] max-h-[600px] -mt-12 justify-self-end mr-2"/>
   </section>
 </template>
 
