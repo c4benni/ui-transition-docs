693bbcfe2ad71e520357040afd35a4cf8268866c
c4benni <c4benni@gmail.com>
2022-01-31 23:56:48 +0100

    Update classNames.ts

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -2,7 +2,7 @@ export default {
   appMaxWidth: "max-w-5xl",
   innerContainerMaxWidth: "sm:max-w-2xl",
   surfaceBG: "bg-surface dark:bg-surface-dark",
-  containerPadding: "px-3 sm:px-6 md:px-0",
+  containerPadding: "px-3 sm:px-6 lg:px-0",
   headline: "text-headline dark:text-headline-dark",
   paragraph: "text-paragraph dark:text-paragraph-dark ",
   divideColor: "border-gray-200 dark:border-gray-800",
