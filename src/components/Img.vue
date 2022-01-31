489eefed2a94306e28e5c49bb11cbbeb3eeaa4bd
c4benni <c4benni@gmail.com>
2022-01-31 12:48:05 +0100

    Update Img.vue

--- src/components/Img.vue
+++ src/components/Img.vue
@@ -1,115 +1,154 @@
 <script lang="ts">
-import { computed, defineComponent, h, ref, resolveComponent, watch } from 'vue'
-import { undefinedProp } from '../utils/main'
-import {Cloudinary} from 'cloudinary-core';
-import Intersection from './Intersection.vue';
+import {
+  computed,
+  defineComponent,
+  h,
+  onBeforeMount,
+  ref,
+  watch,
+} from "vue";
+import { undefinedProp } from "../utils/main";
+import { Cloudinary } from "cloudinary-core";
+import Intersection from "./Intersection.vue";
 
-const cl = new Cloudinary({cloud_name: "c4benn", secure: true});
+const cl = new Cloudinary({ cloud_name: "c4benn", secure: true });
+
+const stringOrNumberProp = undefinedProp([Number, String]);
+
+let mounted = false;
 
 export default defineComponent({
-    emits:['load-start', 'load-success', 'load-error'],
-    props:{
-        publicId: undefinedProp(String),
-        src: undefinedProp(String),
-        alt: undefinedProp(String),
-        width: undefinedProp(Number),
-        height: undefinedProp(Number),
-        quality: undefinedProp([String || Number])
+  emits: ["load-start", "load-success", "load-error"],
+  props: {
+    publicId: undefinedProp(String),
+    src: undefinedProp(String),
+    alt: undefinedProp(String),
+    width: stringOrNumberProp,
+    height: stringOrNumberProp,
+    quality: stringOrNumberProp,
+    loadingBackground: {
+      type: String,
+      default: "bg-card dark:bg-card-dark",
     },
-    setup(p, {attrs, emit}) {
-        const props = computed(()=>p);
-        const loaded = ref(false);
+  },
+  setup(p, { attrs, emit }) {
+    const props = computed(() => p);
+    const loaded = ref(false);
 
-        const intersected = ref(false);
+    onBeforeMount(()=>{
+      mounted = true;
+    })
 
-        const getSrc = computed(()=>{
-            const {publicId, src, height, width, quality} = props.value;
+    const intersected = ref(false);
 
-            if(publicId){
-                return cl.url(`uiTransition/${publicId}`, {
-                    transformation: [
-                        {
-                            height: '300',
-                            width,
-                            quality
-                        }
-                    ]
-                })
-            }
-            return src;
-        })
+    const getSrc = computed(() => {
+      const { publicId, src, height, width, quality } = props.value;
 
-        const loadImage = ()=>{
-            if(!intersected.value) return;
+      if (publicId) {
+        return cl.url(`uiTransition/${publicId}`, {
+          transformation: [
+            {
+              height: "300",
+              width,
+              quality,
+            },
+          ],
+        });
+      }
+      return src;
+    });
 
-            loaded.value =false;
+    const loadImage = () => {
+      if (!intersected.value) return;
 
-            emit('load-start')
+      loaded.value = false;
 
-            const img = new Image();
+      emit("load-start");
 
-            const toggleLoaded = (val:boolean)=>{
-                img.onerror = null;
+      const img = new Image();
 
-                img.onload = null;
+      const toggleLoaded = (val: boolean) => {
+        img.onerror = null;
 
-                loaded.value = val;
+        img.onload = null;
 
-                emit(val ? 'load-success' : 'load-error')
-            }
+        loaded.value = val;
+      };
 
-            img.onload = ()=>{
-                toggleLoaded(true)
-            }
+      img.onload = () => {
+        toggleLoaded(true);
+      };
 
-            img.onerror = ()=>{
-                toggleLoaded(false)
-            }
+      img.onerror = () => {
+        toggleLoaded(false);
+      };
 
-            img.src = getSrc.value
+      img.src = getSrc.value;
+    };
+
+    watch(
+      () => getSrc.value,
+      () => {
+        intersected.value = false;
+
+        loadImage();
+      }
+    );
+
+    watch(
+      () => intersected.value,
+      (n) => {
+        n && loadImage();
+      }
+    );
+
+    return () => {
+      if (loaded.value && mounted) {
+        return h("img", {
+          ...attrs,
+          src: getSrc.value,
+          alt: props.value.alt,
+          height: props.value.height,
+          width: props.value.width,
+          decoding: "async",
+          crossorigin: "anonymous",
+          "data-src-cache": props.value.publicId ? getSrc.value : undefined,
+          onLoad: () => {
+            requestAnimationFrame(() => emit("load-success"));
+          },
+          onError: () => {
+            emit("load-error");
+          },
+        });
+      }
+      return h(
+        Intersection,
+        {
+          disabled: intersected.value,
+        },
+        {
+          default: ({
+            isIntersecting,
+            inactive,
+          }: {
+            inactive: boolean;
+            isIntersecting: boolean;
+          }) => {
+            !inactive && (intersected.value = isIntersecting);
+
+            return h("picture", {
+              ...attrs,
+              title: props.value.alt,
+              class: props.value.loadingBackground,
+              style: {
+                height: `${props.value.height}px`,
+                width: `${props.value.width}px`,
+              },
+            });
+          },
         }
-
-        watch(()=> getSrc.value,()=>{
-            intersected.value = false;
-
-            loadImage()
-        })
-
-        watch(()=> intersected.value,(n)=>{
-            n && loadImage();
-        })
-
-        return ()=>{
-            return h(Intersection,{
-                disabled: intersected.value
-            },{
-                default:({isIntersecting, inactive}:
-                {inactive: boolean; isIntersecting: boolean})=>{
-                    !inactive && (intersected.value = isIntersecting);
-
-                    return loaded.value ?
-                        h('img',{
-                            ...attrs,
-                            src: getSrc.value,
-                            alt: props.value.alt,
-                            height: props.value.height,
-                            width: props.value.width,
-                            decoding: 'async',
-                            crossorigin: 'anonymous',
-                            'data-src-cache': props.value.publicId ? 
-                                getSrc.value: undefined,
-                        })
-                        : h('div',{
-                            ...attrs,
-                            class: 'bg-card dark:bg-card-dark',
-                            style:{
-                                height: `${props.value.height}px`,
-                                width: `${props.value.width}px`,
-                            }
-                        })
-                }
-            })
-        }
-    },
-})
+      );
+    };
+  },
+});
 </script>
