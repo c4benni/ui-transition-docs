278103275dc54aeb618674a1797272baef547577
c4benni <c4benni@gmail.com>
2022-01-31 23:55:25 +0100

    create icons

---
+++ src/components/icons/Css.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24">
+    <path
+      d="M5 3l-.65 3.34h13.59L17.5 8.5H3.92l-.66 3.33h13.59l-.76 3.81l-5.48 1.81l-4.75-1.81l.33-1.64H2.85l-.79 4l7.85 3l9.05-3l1.2-6.03l.24-1.21L21.94 3H5z"
+      fill="currentColor"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CssIcon",
+});
+</script>
