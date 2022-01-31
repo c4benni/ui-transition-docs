1378a9d0400dea91ced0cb2461e6ef7eb961ede9
c4benni <c4benni@gmail.com>
2022-01-31 16:54:14 +0100

    create codeIcon

---
+++ src/components/icons/Code.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 256 256"><path d="M95.972 72.969L34.062 128l61.91 55.031a12 12 0 1 1-15.944 17.938l-72-64a12 12 0 0 1 0-17.938l72-64A12 12 0 1 1 95.972 72.97zm152 46.062l-72-64a12 12 0 1 0-15.944 17.938L221.938 128l-61.91 55.031a12 12 0 1 0 15.944 17.938l72-64a12 12 0 0 0 0-17.938z" fill="currentColor"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'CodeIcon'
+})
+</script>
