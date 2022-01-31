d3d6c06d4ddb3e769beb0428ac961257776959e7
c4benni <c4benni@gmail.com>
2022-01-31 13:44:55 +0100

    added icons

---
+++ src/components/icons/VolumeMute.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="0.52em" height="1em" viewBox="0 0 315 614"><path d="M0 272v150c0 9 7 15 15 15h84l169 129c27 28 47 18 47-20V147c0-39-20-47-47-19L99 257H15c-8 0-15 6-15 15z" fill="currentColor"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'VolumeMute'
+})
+</script>
