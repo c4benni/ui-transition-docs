6f0c2e4c02a97c0ba96f28b3370ed3cb208dda9d
c4benni <c4benni@gmail.com>
2022-01-31 16:54:45 +0100

    Create FeatureCard.vue

---
+++ src/components/sections/Main/Features/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-6 md:p-8 rounded bg-card dark:bg-card-dark">
+      <h2
+        class="text-card-heading dark:text-card-heading-dark font-extrabold text-2xl mb-3 leading-none"
+      >
+          {{title}}
+      </h2>
+
+      <div
+        class="text-card-paragraph dark:text-card-paragraph-dark"
+      >
+          <slot/>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { requiredStringProp } from "../../../../utils/main";
+
+export default defineComponent({
+  name: "Card",
+  props: {
+    title: requiredStringProp,
+  },
+});
+</script>
