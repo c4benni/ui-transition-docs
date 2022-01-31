646a872e69db18bb6045a2a20be4b5a5fc733216
c4benni <c4benni@gmail.com>
2022-01-31 12:48:23 +0100

    update intro/mockup/content

--- src/components/sections/Main/Intro/Mockup/Content.vue
+++ src/components/sections/Main/Intro/Mockup/Content.vue
@@ -1,89 +1,141 @@
 <template>
-      <div
-        class="w-full h-full relative"
+  <div class="w-full h-full relative max-w-full overflow-hidden">
+    <!-- body -->
+    <div class="pt-[24px] px-[12px] w-full pointer-events-none">
+      <p :class="['font-bold text-[1.5em]', classNames.headline]">Browse</p>
+
+      <ul
+        class="hide-scrollbar max-w-full w-full grid grid-flow-col grid-cols-[repeat(2,100%)] gap-x-[6px]"
       >
-            <!-- body -->
-          <div class="pt-[24px] px-[8px]">
+        <LargeCard
+          v-for="item in firstSection"
+          :key="item.title"
+          :class-names="classNames"
+          :badge="item.badge"
+          :primary-color="primaryColor"
+          :title="item.title"
+          :subtitle="item.subtitle"
+          :src="item.src"
+        />
+      </ul>
 
-              <p
-                :class="[
-                    'font-bold text-[1.5em]',
-                    classNames.headline,
-                ]"
-              >
-                  Browse
-              </p>
+      <p
+        :class="[
+            'flex justify-between items-end border-t font-bold pt-[8px]',
+            classNames.divideColor
+        ]"
+      >
+          Top Shows
 
-              <div>
-                  <Img
-                    public-id="intro-mockup/podcast/sneakernomics_xepupg.jpg"
-                    :height="400"
-                    :width="200"
-                  />
-              </div>
+          <span
+            :class="[
+            'font-normal text-[0.85em]',
+                primaryColor
+            ]"
+          >
+              See All
+          </span>
+      </p>
 
-          </div>
+      <ul
+        class="hide-scrollbar max-w-full w-full grid grid-flow-col grid-cols-[repeat(2,calc(50%-4px))] gap-x-[6px]"
+      >
+        <MiniCard
+          v-for="item in secondSection"
+          :key="item.title"
+          :class-names="classNames"
+          :primary-color="primaryColor"
+          :title="item.title"
+          :subtitle="item.subtitle"
+          :src="item.src"
+        />
+      </ul>
+    </div>
 
-          <!-- overlay -->
+    <!-- overlay -->
 
-          <div class="absolute bottom-0 w-full h-full pb-[16px]">
-              <!-- drawer -->
-              <div
-                class="h-[calc(100%-1rem)] bottom-[16px] absolute w-full translate-y-[calc(100%-96px)]"
-              >
+    <div class="absolute bottom-0 w-full h-full">
+      <!-- drawer -->
+      <Drawer
+        :class-names="classNames"
+        :primary-color="primaryColor"
+        :expanded="expanded"
+        @toggle-expand="e => expanded = e"
+      />
 
-              </div>
-
-              <!-- tabs -->
-              <ul
-                :class="[
-                    'h-[48px] absolute bottom-[16px] w-full border-t-[0.75px] flex justify-around',
-                    classNames.divideColor
-                ]"
-              >
-                <IconWrapper
-                    v-for="(icon, i) in navIcons"
-                    :key="icon"
-                    tag="li"
-                    :class="[
-                        'text-[19px]',
-                        {
-                            'opacity-70': i !== 0,
-                            'text-blue-700 dark:text-blue-500': i ===0
-                        }
-                    ]"
-                >
-                    <Component :is="icon"/>
-                </IconWrapper>
-              </ul>
-          </div>
-      </div>
+      <!-- tabs -->
+      <Tabs
+        :class-names="classNames"
+        :primary-color="primaryColor"
+        :expanded="expanded"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import IconWrapper from '../../../../icons/IconWrapper.vue'
-import PlayFilledIcon from '../../../../icons/PlayFilled.vue'
-import CollectionsIcon from '../../../../icons/Collections.vue'
-import SearchIcon from '../../../../icons/Search.vue'
-import classNames from '../../../../../utils/classNames'
-import Img from '../../../../Img.vue'
+import { defineComponent, ref } from "vue";
+import classNames from "../../../../../utils/classNames";
+import LargeCard from "./LargeCard.vue";
+import MiniCard from "./MiniCard.vue";
+import Tabs from "./Tabs.vue";
+import Drawer from "./Drawer/index.vue";
 
 export default defineComponent({
-    name: "MockupContent",
-    components: { IconWrapper, PlayFilledIcon, CollectionsIcon, SearchIcon, Img },
+  name: "MockupContent",
+  components: {
+    LargeCard,
+    MiniCard,
+    Tabs,
+    Drawer
+},
 
-    setup(){
-      const navIcons = [
-          'PlayFilledIcon',
-          'CollectionsIcon',
-          'SearchIcon'
-      ]
+  setup() {
+    const primaryColor = "text-blue-700 dark:text-blue-500";
 
-      return {
-          navIcons,
-          classNames
-      }
-    }
-})
+    const expanded = ref(false);
+
+    const firstSection = [
+      {
+        badge: "Featured",
+        title: "Sneakernomics",
+        subtitle: "The cost of clean kicks",
+        src: "intro-mockup/podcast/sneakernomics_xepupg.jpg",
+      },
+      {
+        badge: "New Show",
+        title: "Dear Daughter",
+        src: "intro-mockup/podcast/dear-daughter_jizxah.jpg",
+      },
+    ];
+
+    const secondSection = [
+        {
+            title: 'AMPD Creative Industry Podcast with Nasty C',
+            subtitle: 'AMPD STUDIOS by Old Mutual',
+            src:'intro-mockup/podcast/nasty-c_gyhh64.jpg'
+        },
+
+        {
+            title: 'The Midnight Miracle',
+            subtitle: 'Dave Chapelle, Talib Kweli, Yasiin Bey',
+            src:'intro-mockup/podcast/midnight-miracle_ywhj4p.jpg'
+        },
+
+        {
+            title: 'Culture Crave',
+            subtitle: 'Culture Crave',
+            src:'intro-mockup/podcast/culture-crave_z2ftsu.jpg'
+        }
+    ]
+
+    return {
+      primaryColor,
+      classNames,
+      firstSection,
+      secondSection,
+      expanded
+    };
+  },
+});
 </script>
