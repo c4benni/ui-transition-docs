172c084332f1ca999399fea3a07ffa1672b50f45
c4benni <c4benni@gmail.com>
2022-01-31 16:54:56 +0100

    create Features

---
+++ src/components/sections/Main/Features/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <section
+        class="mt-16 xs:px-[0.375rem] md:px-0 max-w-lg md:max-w-full mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 mb-16"
+    >
+        <Card
+            v-for="feature in features"
+            :key="feature.title"
+            :title="feature.title"
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi molestiae nisi fugiat ex quam eveniet commodi ad reprehenderit non accusamus. Error consequuntur, beatae voluptatem sed illo voluptatibus distinctio atque quisquam.
+        </Card>
+
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from './Card.vue'
+
+export default defineComponent({
+    name: "Features",
+    components: { Card },
+
+    setup(){
+        const feature = (title:string, body:string) =>({
+            title,body
+        })
+
+        const features = [
+            feature('60fps', ''),
+            feature('Dynamic', ''),
+            feature('Optimized', ''),
+            feature('Open Source', '')
+        ]
+
+        return {
+            features
+        }
+    }
+})
+</script>
