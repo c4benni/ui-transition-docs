eedfc5242ca8ee0204ef9b0610b8fbe443d7f78b
c4benni <c4benni@gmail.com>
2022-02-01 11:23:46 +0100

    Create RoundActionBtn.vue

---
+++ src/components/Inputs/RoundActionBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <Button size="sm" class="gap-x-1 rounded-full">
+    {{ title }}
+    <IconWrapper>
+      <ChevronRight />
+    </IconWrapper>
+  </Button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+import IconWrapper from "../icons/IconWrapper.vue";
+import ChevronRight from "../icons/ChevronRight.vue";
+
+export default defineComponent({
+  name: "RoundActionBtn",
+  components: { Button, IconWrapper, ChevronRight },
+  props: {
+    title: {
+      type: String,
+      default: "Get started",
+    },
+  },
+});
+</script>
