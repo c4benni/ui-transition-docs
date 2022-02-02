16842df08c71ab20e1e367033d9b276f7de3254c
c4benni <c4benni@gmail.com>
2022-02-02 19:13:04 +0100

    create customizable wireframes wrapper

---
+++ src/components/sections/Main/Customizable/Playground/Visuals/Wireframe/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :class="[
+      'w-full overflow-hidden',
+      {
+        'h-full': !breakpoint.isMobile,
+        [`h-[min(320px,70vh)] mt-2 mb-9 rounded-lg border ${classNames.divideColor}`]:
+          breakpoint.isMobile,
+      },
+    ]"
+  >
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import useBreakpoint from "../../../../../../../hooks/useBreakpoints";
+import classNames from "../../../../../../../utils/classNames";
+
+export default defineComponent({
+  name: "CustomizableWireframe",
+  setup() {
+    const breakpoint = useBreakpoint();
+    return { breakpoint, classNames };
+  },
+});
+</script>
