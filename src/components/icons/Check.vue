0ab635cf62cf16fcdd3f349e45964fbf8b1f302f
c4benni <c4benni@gmail.com>
2022-02-02 10:36:25 +0100

    added icons

---
+++ src/components/icons/Check.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24"><path d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59L21 7z" fill="currentColor"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'CheckIcon'
+})
+</script>
