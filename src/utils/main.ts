ad670d02d3a569c1b9082ff126014f5230b3602d
c4benni <c4benni@gmail.com>
2022-02-02 10:37:20 +0100

    update utils/main
    added copy utils

--- src/utils/main.ts
+++ src/utils/main.ts
@@ -1,7 +1,9 @@
+type MediaListener = (arg: {
+  media: MediaQueryList;
+  callback: (e: MediaQueryListEvent) => void;
+}) => void;
 
-type MediaListener = (arg: {media: MediaQueryList, callback: (e: MediaQueryListEvent)=> void}) => void
-
-export const mediaListener :MediaListener= ({ media, callback }) => {
+export const mediaListener: MediaListener = ({ media, callback }) => {
   try {
     media.addEventListener("change", callback);
   } catch (e: any) {
@@ -11,7 +13,6 @@ export const mediaListener :MediaListener= ({ media, callback }) => {
     }
   }
 };
-
 
 export function isHTML(el: any) {
   return el instanceof HTMLElement;
@@ -35,3 +36,57 @@ export const requiredProp = (type: any | any[]) => ({
 
 export const undefinedStringProp = undefinedProp(String);
 export const requiredStringProp = requiredProp(String);
+
+export function copy(arg: {
+  text: string;
+  done?: () => void;
+  onSuccess: () => void;
+  onError: () => void;
+}): Promise<boolean> {
+  const {
+    text,
+    done = () => {},
+    onSuccess = () => {},
+    onError = () => {},
+  } = arg;
+
+  return new Promise((r) => {
+    const oldBrowser = () => {
+      try {
+        let psuedoInput = document.createElement("input");
+        psuedoInput.classList.add("sr-only");
+        psuedoInput.setAttribute("tabindex", "-1");
+        psuedoInput.setAttribute("aria-hidden", "true");
+        psuedoInput.value = text;
+        document.body.appendChild(psuedoInput);
+        psuedoInput.select();
+
+        document.execCommand("copy");
+        document.body.removeChild(psuedoInput);
+        onSuccess();
+        r(true);
+      } catch (e) {
+        if (e) {
+          onError();
+          r(false);
+        }
+      } finally {
+        done();
+      }
+    };
+
+    if ("clipboard" in navigator) {
+      try {
+        navigator.clipboard.writeText(text);
+        onSuccess();
+        done();
+
+        r(true);
+      } catch (e) {
+        if (e) {
+          oldBrowser();
+        }
+      }
+    } else oldBrowser();
+  });
+}
