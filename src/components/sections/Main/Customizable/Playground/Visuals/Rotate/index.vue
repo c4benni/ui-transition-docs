d0cc7da9a69228574d629b734fe761f3b500789f
c4benni <c4benni@gmail.com>
2022-02-02 23:36:43 +0100

    update customizable rotate visuals

--- src/components/sections/Main/Customizable/Playground/Visuals/Rotate/index.vue
+++ src/components/sections/Main/Customizable/Playground/Visuals/Rotate/index.vue
@@ -1,16 +1,87 @@
 <template>
-  <div>
-      360
-  </div>
+  <Wireframe class="flex flex-col">
+    <ul
+      class="flex border-y border-gray-200 dark:border-gray-800 h-fit rounded-t-[inherit]"
+    >
+      <Button
+        v-for="(tab, i) in tabs"
+        :key="tab"
+        tag="li"
+        tabindex="-1"
+        size="sm"
+        :class="[
+          'scale-100 translate-y-0 flex-shrink-0 flex-grow rounded-none clip-none text-[14px] h-[42px] md:h-[36px]',
+          {
+            'rounded-tl-[inherit]': i === 0,
+            'border-l border-l-gray-200 dark:border-l-gray-800 rounded-tr-[inherit]':
+              i === 1,
+            'border-b-[1.5px] border-primary text-primary dark:border-primary-dark dark:text-primary-dark':
+              i === rotateState,
+            'opacity-60 can-hover:hover:opacity-80': rotateState !== i,
+          },
+        ]"
+        :primary="false"
+        @click="changeTab(i)"
+      >
+        {{ tab }}
+      </Button>
+    </ul>
+
+    <p class="text-paragraph dark:text-paragraph-dark px-[16px] mt-[24px]">
+      Change tabs and watch Fab below for effect.
+    </p>
+
+    <Button
+      class="w-[56px] h-[56px] rounded-lg text-2xl absolute right-[20px] bottom-[20px] md:bottom-[42px] md:right-[16px] translate-y-0 scale-100 shadow-2xl clip-none"
+    >
+      <IconWrapper>
+        <Component :is="rotateState === 0 ? 'Filter' : 'NewMessage'" />
+      </IconWrapper>
+    </Button>
+  </Wireframe>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from "vue";
+import Wireframe from "../Wireframe/index.vue";
+import GenericWireframe from "../Wireframe/Generic.vue";
+import { visualsState } from "../../Snippet/state";
+import IconWrapper from "../../../../../../icons/IconWrapper.vue";
+import Check from "../../../../../../icons/Check.vue";
+import Close from "../../../../../../icons/Close.vue";
+import Button from "../../../../../../Inputs/Button.vue";
+import NewMessage from "../../../../../../icons/NewMessage.vue";
+import Filter from "../../../../../../icons/Filter.vue";
 
 export default defineComponent({
-    name:'CustomizableRotateVisuals',
-    setup() {
-        
-    },
-})
+  name: "CustomizableRotateVisuals",
+  components: {
+    Wireframe,
+    GenericWireframe,
+    IconWrapper,
+    Check,
+    Close,
+    Button,
+    NewMessage,
+    Filter
+},
+
+  setup() {
+    const rotateState = computed(() => visualsState.value.Rotate);
+
+    const tabs = ["Tab 1", "Tab 2"];
+
+    const changeTab = (tab: number) =>
+      (visualsState.value = {
+        ...visualsState.value,
+        Rotate: tab,
+      });
+
+    return {
+      rotateState,
+      tabs,
+      changeTab,
+    };
+  },
+});
 </script>
