9aab40c6ea4aa075306f7b947423e83507536762
c4benni <c4benni@gmail.com>
2022-02-02 22:39:50 +0100

    update customizable translateY visuals

--- src/components/sections/Main/Customizable/Playground/Visuals/TranslateY/index.vue
+++ src/components/sections/Main/Customizable/Playground/Visuals/TranslateY/index.vue
@@ -1,16 +1,71 @@
 <template>
-  <Wireframe>
-    <GenericWireframe action-text="Open toast" />
+  <Wireframe class="grid">
+    <GenericWireframe
+      :disable-action="toastState"
+      action-text="Open toast"
+      @open-visual="openToast"
+    />
+
+    <div
+      v-if="toastState"
+      :class="[
+        'grid justify-self-center bg-white dark:bg-card-dark w-[calc(100%-16px)] md:w-[calc(100%-12px)] border border-gray-300/50 dark:border-gray-600/30 isolate absolute w-h-full top-[8px] md:top-[20px] shadow-2xl dark:shadow-xl rounded-[8px] p-[12px] md:px-[10px] h-[56px] md:h-fit grid-flow-col grid-cols-[20px,1fr] font-medium gap-x-[8px] items-center',
+      ]"
+    >
+      <IconWrapper
+        class="w-[20px] h-[20px] rounded-full bg-green-200/40 dark:bg-surface-dark text-green-700 dark:text-green-500"
+      >
+        <Check/>
+      </IconWrapper>
+
+      Item saved!
+
+      <Button
+        tag="div"
+        icon
+        title="Close toast"
+        class="w-[28px] h-[28px] md:w-[24px] md:h-[24px] text-sm translate-y-0 scale-100"
+        @click="closeToast"
+      >
+        <Close/>
+      </Button>
+    </div>
   </Wireframe>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Wireframe from "../Wireframe/index.vue";
 import GenericWireframe from "../Wireframe/Generic.vue";
+import { visualsState } from "../../Snippet/state";
+import IconWrapper from "../../../../../../icons/IconWrapper.vue";
+import Check from "../../../../../../icons/Check.vue";
+import Close from "../../../../../../icons/Close.vue";
+import Button from "../../../../../../Inputs/Button.vue";
 
 export default defineComponent({
-    name: "CustomizableTranslateYVisuals",
-    components: { Wireframe, GenericWireframe }
+  name: "CustomizableTranslateYVisuals",
+  components: { Wireframe, GenericWireframe, IconWrapper, Check, Close, Button },
+
+  setup() {
+    const toastState = computed(() => visualsState.value.TranslateY);
+
+    const toggleToast = (val: boolean) => {
+      visualsState.value = {
+        ...visualsState.value,
+        TranslateY: val,
+      };
+    };
+
+    const closeToast = () => toggleToast(false);
+
+    const openToast = () => toggleToast(true);
+
+    return {
+      toastState,
+      closeToast,
+      openToast,
+    };
+  },
 });
 </script>
