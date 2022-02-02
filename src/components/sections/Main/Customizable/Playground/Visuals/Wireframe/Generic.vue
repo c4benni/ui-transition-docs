699e87f083ea3a8725cd452863d09bfd3ad99aa2
c4benni <c4benni@gmail.com>
2022-02-02 22:40:11 +0100

    update customizable Generic wireframe

--- src/components/sections/Main/Customizable/Playground/Visuals/Wireframe/Generic.vue
+++ src/components/sections/Main/Customizable/Playground/Visuals/Wireframe/Generic.vue
@@ -1,64 +1,67 @@
 <template>
+  <div
+    :class="[
+      'h-full w-full grid',
+      {
+        'items-center': breakpoint.isMobile,
+        'grid-rows-[40%,auto] pt-6 px-2': !breakpoint.isMobile,
+      },
+    ]"
+  >
+    <!-- image -->
     <div
-      :class="[
-        'h-full w-full grid',
-        {
-          'items-center': breakpoint.isMobile,
-          'grid-rows-[40%,auto] pt-6 px-2': !breakpoint.isMobile,
-        },
-      ]"
-    >
-      <!-- image -->
-      <div
-        v-if="!breakpoint.isMobile"
-        class="h-full w-full rounded-md bg-current opacity-[0.125]"
-      />
+      v-if="!breakpoint.isMobile"
+      class="h-full w-full rounded-md bg-current opacity-[0.125]"
+    />
 
-      <div class="flex flex-col">
-        <template v-if="!breakpoint.isMobile">
-          <!-- price -->
-          <div
-            class="rounded-full h-[12px] w-[30%] mt-[10px] bg-current opacity-10"
-          />
+    <div class="flex flex-col">
+      <template v-if="!breakpoint.isMobile">
+        <!-- price -->
+        <div
+          class="rounded-full h-[12px] w-[30%] mt-[10px] bg-current opacity-10"
+        />
 
-          <!-- title -->
-          <div
-            class="rounded-full h-[18px] w-[68%] mt-[10px] bg-current opacity-10"
-          />
+        <!-- title -->
+        <div
+          class="rounded-full h-[18px] w-[68%] mt-[10px] bg-current opacity-10"
+        />
 
-          <!-- desc -->
-          <div
-            v-for="i in 4"
-            :key="i"
-            :class="[
-              'rounded-full h-[10px] bg-current opacity-10',
-              {
-                'w-[38%] mt-[32px]': i === 1,
-                'mt-[12px] w-[82%]': i === 2,
-                'mt-[8px] w-[80%]': i === 3,
-                'mt-[8px] w-[52%]': i === 4,
-              },
-            ]"
-          />
-
-          <div class="flex-grow" />
-        </template>
-
-        <!-- action -->
-        <Button
-          size="sm"
+        <!-- desc -->
+        <div
+          v-for="i in 4"
+          :key="i"
           :class="[
-            'translate-y-0 justify-self-center mx-[12px]',
+            'rounded-full h-[10px] bg-current opacity-10',
             {
-              'mb-[42px] text-[14px]': !breakpoint.isMobile,
-              'self-center px-6': breakpoint.isMobile,
+              'w-[38%] mt-[32px]': i === 1,
+              'mt-[12px] w-[82%]': i === 2,
+              'mt-[8px] w-[80%]': i === 3,
+              'mt-[8px] w-[52%]': i === 4,
             },
           ]"
-        >
-          {{actionText}}
-        </Button>
-      </div>
+        />
+
+        <div class="flex-grow" />
+      </template>
+
+      <!-- action -->
+      <Button
+        size="sm"
+        tag="div"
+        :disabled="disableAction"
+        :class="[
+          'translate-y-0 justify-self-center mx-[12px]',
+          {
+            'mb-[42px] text-[14px]': !breakpoint.isMobile,
+            'self-center px-6': breakpoint.isMobile,
+          },
+        ]"
+        @click="$emit('open-visual')"
+      >
+        {{ actionText }}
+      </Button>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -70,11 +73,13 @@ import Button from "../../../../../../Inputs/Button.vue";
 
 export default defineComponent({
   name: "CustomizableGenericWireframes",
-  props:{
-    actionText: requiredStringProp
+  emits: ["open-visual"],
+  props: {
+    actionText: requiredStringProp,
+    disableAction: Boolean,
   },
   setup() {
-    const breakpoint = useBreakpoint()
+    const breakpoint = useBreakpoint();
 
     return { breakpoint, classNames };
   },
