d425907538a268464a3f6222006d423b7fd78ab7
c4benni <c4benni@gmail.com>
2022-02-02 23:36:12 +0100

    create icons

---
+++ src/components/icons/Plus.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="currentColor"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'PlusIcon'
+})
+</script>
