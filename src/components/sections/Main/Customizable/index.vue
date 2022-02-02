019b83a309fb0264e2b6389a6cbd2ed44353372f
c4benni <c4benni@gmail.com>
2022-02-02 19:10:32 +0100

    update customizable
    Changed bg-color

--- src/components/sections/Main/Customizable/index.vue
+++ src/components/sections/Main/Customizable/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class="w-full relative fill-before isolate before:-z-1 before:bg-card/20 dark:before:bg-card-dark/20 before:w-screen before:left-[50%] before:translate-x-[-50%] before:border-y before:border-gray-300/40 dark:before:border-gray-700/20 py-6 sm:py-12 xs:px-[0.375rem] sm:px-0  "
+        class="w-full relative fill-before isolate before:-z-1 before:bg-[hsl(240,20%,99%)] dark:before:bg-[hsl(216,6%,10%)] before:w-screen before:left-[50%] before:translate-x-[-50%] before:border-y before:border-gray-300/40 dark:before:border-gray-700/20 py-6 sm:py-12 xs:px-[0.375rem] sm:px-0"
     >
         <FeatureIntro
             badge="Customizable"
