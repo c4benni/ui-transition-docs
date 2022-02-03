429ed0a9056fb9fd3372c872695551ae955f6cbd
c4benni <c4benni@gmail.com>
2022-02-03 15:45:12 +0100

    update customizable SnippetTabs

--- src/components/sections/Main/Customizable/Playground/Snippet/Tabs.vue
+++ src/components/sections/Main/Customizable/Playground/Snippet/Tabs.vue
@@ -1,33 +1,25 @@
 <template>
-  <ul class="pb-3 overflow-x-auto hide-scrollbar flex items-center h-[72px] fill-after after:h-[72px] after:w-[32px] after:left-auto after:right-[-12px] after:top-auto after-gradient pr-[34px]">
-    <template v-for="(snippet, i) in snippets" :key="snippet.title">
-      <Button
-        :primary="false"
-        tag="li"
-        :class="[
-          'grid-flow-row justify-items-center text-sm gap-y-1 translate-y-0 h-[56px] py-1 flex-grow flex-shrink-0 scale-100',
-          {
-            'text-paragraph dark:text-paragraph-dark opacity-70 dark:opacity-60 can-hover:hover:opacity-80':
-              activeSnippet !== snippet.icon,
-            'opacity-100':
-              activeSnippet === snippet.icon,
-          },
-        ]"
-        @click="changeSnippet(snippet.icon)"
-      >
-        <IconWrapper class="text-2xl">
-          <Component :is="snippet.icon" />
-        </IconWrapper>
-
-        {{ snippet.title }}
-      </Button>
-
-      <li
-        v-if="i <= snippets.length - 2"
-        class="h-[65%] rounded-full w-[2px] bg-current opacity-20 pointer-events-none flex-shrink-0"
-        aria-hidden="true"
-      />
-    </template>
+  <ul
+    class="pb-3 overflow-x-auto hide-scrollbar flex items-center h-[56px] fill-after after:h-[72px] after:w-[32px] after:left-auto after:right-[-12px] after:top-auto after-gradient pr-[34px]"
+  >
+    <Button
+      v-for="(snippet, i) in snippets"
+      :key="snippet.title"
+      :primary="false"
+      size="sm"
+      tag="li"
+      :class="[
+        'grid-flow-row justify-items-center text-sm gap-y-1 translate-y-0 py-1 flex-shrink-0 scale-100 rounded-full mr-2',
+        {
+          'text-paragraph/60 dark:text-paragraph-dark/60 can-hover:hover:text-paragraph/100 dark:can-hover:hover:text-paragraph-dark/100':
+            activeSnippet !== snippet.icon,
+          [classNames.activeTab]: activeSnippet === snippet.icon,
+        },
+      ]"
+      @click="changeSnippet(snippet.icon)"
+    >
+      {{ snippet.title }}
+    </Button>
   </ul>
 </template>
 
@@ -40,6 +32,7 @@ import Rotate from "../../../../../icons/Rotate.vue";
 import Button from "../../../../../Inputs/Button.vue";
 import IconWrapper from "../../../../../icons/IconWrapper.vue";
 import { activeSnippet, activeTab, Snippet } from "./state";
+import classNames from "../../../../../../utils/classNames";
 
 export default defineComponent({
   name: "Tabs",
@@ -47,10 +40,11 @@ export default defineComponent({
   components: { Scale, TranslateX, TranslateY, Rotate, Button, IconWrapper },
 
   setup(_, { emit }) {
-    const snippet = (icon: string, title?: string) => ({
-      icon,
-      title: title || icon,
-    } as  {icon: Snippet, title: string});
+    const snippet = (icon: string, title?: string) =>
+      ({
+        icon,
+        title: title || icon,
+      } as { icon: Snippet; title: string });
 
     const snippets = [
       snippet("Scale"),
@@ -62,12 +56,13 @@ export default defineComponent({
     const changeSnippet = (tab: Snippet) => {
       activeSnippet.value = tab;
 
-      activeTab.value = 'App.vue'
+      activeTab.value = "App.vue";
 
       emit("tab-changed", tab);
     };
 
     return {
+      classNames,
       snippets,
       activeSnippet,
       changeSnippet,
@@ -77,11 +72,19 @@ export default defineComponent({
 </script>
 
 <style scoped lang="postcss">
-  .after-gradient::after{
-    background-image: linear-gradient(to right, hsl(240,20%,99%,0), hsl(240,20%,99%) 30%);
-  }
+.after-gradient::after {
+  background-image: linear-gradient(
+    to right,
+    hsl(0, 0%, 100%, 0),
+    hsl(0, 0%, 100%)
+  );
+}
 
-  .dark .after-gradient::after{
-    background-image: linear-gradient(to right, hsl(216,6%,10%,0), hsl(216,6%,10%) 30%);
-  }
+.dark .after-gradient::after {
+  background-image: linear-gradient(
+    to right,
+    hsl(180, 5%, 4%, 0),
+    hsl(180, 5%, 4%)
+  );
+}
 </style>
