fb51c0fe8bebfb0e52561d8a1fd5f702e64d7df0
c4benni <c4benni@gmail.com>
2022-02-03 17:03:09 +0100

    update controllable/bottomSheet

--- src/components/sections/Main/Controllable/Playground/Visuals/BottomSheet/index.vue
+++ src/components/sections/Main/Controllable/Playground/Visuals/BottomSheet/index.vue
@@ -1,14 +1,35 @@
 <template>
-  <Wireframe class="flex flex-col">
+  <Wireframe class="flex flex-col isolate">
     <Img
       public-id="ControllableMockup/google_street_view_uq1tpc.png"
       class="absolute h-[calc(100%-186px)] w-full rounded-[inherit] top-0 left-0 object-cover object-center scale-125 origin-bottom"
     />
+
+    <Button
+      tag="span"
+      tabindex="-1"
+      class="text-black dark:text-white w-[32px] h-[32px] rounded-full shadow-xl translate-y-0 scale-100 pointer-events-none absolute z-1 top-[8px] left-[8px] clip-none bg-white dark:bg-card-dark"
+    >
+      <IconWrapper>
+        <Menu />
+      </IconWrapper>
+    </Button>
+
+    <div
+      :class="[
+        'absolute top-0 left-0 w-h-full z-1 rounded-[inherit]',
+        {
+          'pointer-events-none': true,
+        },
+      ]"
+    >
+      <TopDialog/>
+    </div>
   </Wireframe>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, VNode } from "vue";
 import Wireframe from "../Wireframe/index.vue";
 import GenericWireframe from "../Wireframe/Generic.vue";
 import { visualsState } from "../../Snippet/state";
@@ -19,6 +40,8 @@ import Button from "../../../../../../Inputs/Button.vue";
 import NewMessage from "../../../../../../icons/NewMessage.vue";
 import Phone from "../../../../../../icons/Phone.vue";
 import Img from "../../../../../../Img.vue";
+import Menu from "../../../../../../icons/Menu.vue";
+import TopDialog from "./TopDialog.vue";
 
 export default defineComponent({
   name: "CustomizableRotateVisuals",
@@ -31,7 +54,9 @@ export default defineComponent({
     Button,
     NewMessage,
     Phone,
-    Img
+    Img,
+    Menu,
+    TopDialog
 },
 
   setup() {
@@ -44,6 +69,7 @@ export default defineComponent({
         ...visualsState.value,
         Rotate: tab,
       });
+
 
     return {
       rotateState,
