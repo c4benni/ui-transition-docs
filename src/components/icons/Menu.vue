4d645692bc58f3312f00ad43346b8cc5491fdccf
c4benni <c4benni@gmail.com>
2022-02-03 15:42:52 +0100

    update Icons

---
+++ src/components/icons/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 512 512">
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="48"
+      d="M88 152h336"
+    ></path>
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="48"
+      d="M88 256h336"
+    ></path>
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="48"
+      d="M88 360h336"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuIcon",
+});
+</script>
