184586ccc4dfade97356bbd85e9766d64a52178f
c4benni <c4benni@gmail.com>
2022-02-03 15:45:25 +0100

    update customizable visuals rotate

--- src/components/sections/Main/Customizable/Playground/Visuals/Rotate/index.vue
+++ src/components/sections/Main/Customizable/Playground/Visuals/Rotate/index.vue
@@ -15,7 +15,7 @@
             'rounded-tl-[inherit]': i === 0,
             'border-l border-l-gray-200 dark:border-l-gray-800 rounded-tr-[inherit]':
               i === 1,
-            'border-b-[1.5px] border-primary text-primary dark:border-primary-dark dark:text-primary-dark':
+            'font-bold border-b-[1.5px] border-primary text-primary dark:border-primary-dark dark:text-primary-dark':
               i === rotateState,
             'opacity-60 can-hover:hover:opacity-80': rotateState !== i,
           },
@@ -28,14 +28,15 @@
     </ul>
 
     <p class="text-paragraph dark:text-paragraph-dark px-[16px] mt-[24px]">
-      Change tabs and watch Fab below for effect.
+      Switch tabs and watch Fab below for effect.
     </p>
 
     <Button
-      class="w-[56px] h-[56px] rounded-lg text-2xl absolute right-[20px] bottom-[20px] md:bottom-[42px] md:right-[16px] translate-y-0 scale-100 shadow-2xl clip-none"
+      class="w-[56px] h-[56px] rounded-[16px] text-2xl absolute right-[20px] bottom-[20px] md:bottom-[42px] md:right-[16px] translate-y-0 scale-100 shadow-xl clip-none bg-green-600 dark:bg-green-500 text-white"
+      :primary="false"
     >
       <IconWrapper>
-        <Component :is="rotateState === 0 ? 'Filter' : 'NewMessage'" />
+        <Component :is="rotateState === 0 ? 'Phone' : 'NewMessage'" />
       </IconWrapper>
     </Button>
   </Wireframe>
@@ -51,7 +52,7 @@ import Check from "../../../../../../icons/Check.vue";
 import Close from "../../../../../../icons/Close.vue";
 import Button from "../../../../../../Inputs/Button.vue";
 import NewMessage from "../../../../../../icons/NewMessage.vue";
-import Filter from "../../../../../../icons/Filter.vue";
+import Phone from "../../../../../../icons/Phone.vue";
 
 export default defineComponent({
   name: "CustomizableRotateVisuals",
@@ -63,13 +64,13 @@ export default defineComponent({
     Close,
     Button,
     NewMessage,
-    Filter
+    Phone
 },
 
   setup() {
     const rotateState = computed(() => visualsState.value.Rotate);
 
-    const tabs = ["Tab 1", "Tab 2"];
+    const tabs = ["Call", "Message"];
 
     const changeTab = (tab: number) =>
       (visualsState.value = {
