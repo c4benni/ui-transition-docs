7a5742467b7bd52c15a629093040a46492ce5ef3
c4benni <c4benni@gmail.com>
2022-02-04 21:59:59 +0100

    update GroupsAndDelays Visuals

--- src/components/sections/Main/GroupsAndDelays/Playground/Visuals/index.vue
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/index.vue
@@ -6,12 +6,13 @@
 import { computed, defineComponent } from "vue";
 import Fabs from "./Fabs/index.vue";
 import NavMenu from "./NavMenu/index.vue";
+import ShareDialog from "./ShareDialog/index.vue";
 import { activeSnippet } from "../Snippet/state";
 import Tabs from "../Snippet/Tabs.vue";
 
 export default defineComponent({
   name: "CustomizableVisuals",
-  components: { Fabs, NavMenu, Tabs },
+  components: { Fabs, NavMenu, Tabs, ShareDialog },
   setup() {
     const render = computed(()=>{
       return activeSnippet.value
