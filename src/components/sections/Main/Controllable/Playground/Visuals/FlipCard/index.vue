7e087a1a4d0f723c1c85d3135bfaef3d31e9c617
c4benni <c4benni@gmail.com>
2022-02-04 13:40:59 +0100

    Create index.vue

---
+++ src/components/sections/Main/Controllable/Playground/Visuals/FlipCard/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <Wireframe class="py-[12px] flex flex-col justify-center">
+    <div
+      :class="[
+        'card',
+        {
+          active: flipped,
+        },
+      ]"
+      @click="flipped = !flipped"
+    >
+      <div class="content">
+        <div class="front bg-green-500">Front</div>
+        <div class="back bg-red-500">Back!</div>
+      </div>
+    </div>
+
+    <Button
+      tag="div"
+      tabindex="-1"
+      class="translate-y-0 w-fit self-center mt-[16px] text-[14px] scale-100 px-[16px]"
+      size="sm"
+      @click="flipped = !flipped"
+    >
+      Flip card
+    </Button>
+  </Wireframe>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+import Wireframe from "../Wireframe/index.vue";
+import GenericWireframe from "../Wireframe/Generic.vue";
+import Button from "../../../../../../Inputs/Button.vue";
+import { visualsState } from "../../Snippet/state";
+
+export default defineComponent({
+  name: "ControllableFlipCardVisuals",
+  components: { Wireframe, GenericWireframe, Button },
+
+  setup() {
+    const flipped = ref(false);
+
+    return {
+      flipped,
+    };
+  },
+});
+</script>
+
+<style scoped lang="postcss">
+.card {
+  position: relative;
+  width: calc(100% - 1rem);
+  height: 50%;
+  margin: 0 auto;
+  perspective: 500px;
+}
+
+.content {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transition-duration: 500ms;
+}
+
+.card.active .content {
+  transform: rotateX(180deg);
+}
+
+.front,
+.back {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  border-radius: 5px;
+  backface-visibility: hidden;
+}
+
+.back {
+  transform: rotateX(180deg);
+}
+</style>
