c26c7fd7d344603980fab13efbec0d831291f138
c4benni <c4benni@gmail.com>
2022-02-04 13:41:56 +0100

    Create index.vue

---
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Component :is="render"/>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import Fabs from "./Fabs/index.vue";
+import NavMenu from "./NavMenu/index.vue";
+import { activeSnippet } from "../Snippet/state";
+import Tabs from "../Snippet/Tabs.vue";
+
+export default defineComponent({
+  name: "CustomizableVisuals",
+  components: { Fabs, NavMenu, Tabs },
+  setup() {
+    const render = computed(()=>{
+      return activeSnippet.value
+    })
+
+    return {
+      render
+    }
+  },
+});
+</script>
