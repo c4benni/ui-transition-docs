d0fd7fb6b9b0f0eb7aa31c09bfe6ccc8619db86d
c4benni <c4benni@gmail.com>
2022-02-04 13:42:06 +0100

    Create index.vue

---
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Switch/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Wireframe>
+    <div
+      class="flex items-center justify-center h-full"
+    >
+      <label for="check" class="mr-[8px]"> Toggle </label>
+      <Switch id="check" />
+    </div>
+  </Wireframe>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import Wireframe from "../Wireframe/index.vue";
+import { visualsState } from "../../Snippet/state";
+import Switch from "../../../../../../Inputs/Switch.vue";
+
+export default defineComponent({
+  name: "CustomizableScaleVisuals",
+  components: { Wireframe, Switch },
+
+  setup() {},
+});
+</script>
