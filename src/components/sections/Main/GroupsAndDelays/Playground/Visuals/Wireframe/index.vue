d24ad28e1f8a30644c01b78a2c8acf652f0fd82a
c4benni <c4benni@gmail.com>
2022-02-06 15:21:28 +0100

    update GroupAndDelays wireframe

--- src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Wireframe/index.vue
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Wireframe/index.vue
@@ -1,28 +1,15 @@
 <template>
-  <div
-    :class="[
-      'w-full overflow-hidden relative',
-      {
-        'h-full': !breakpoint.isMobile,
-        [`h-[min(320px,85vh)] xxs:landscape:h-[min(320px,85vh)] xs:h-[min(500px,70vh)] max-w-[424px] mx-auto mt-2 mb-9 rounded-lg border ${classNames.divideColor}`]:
-          breakpoint.isMobile,
-      },
-    ]"
-  >
+  <Wireframe>
     <slot/>
-  </div>
+  </Wireframe>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import useBreakpoint from "../../../../../../../hooks/useBreakpoints";
-import classNames from "../../../../../../../utils/classNames";
+import Wireframe from "../../../../Wireframe.vue";
 
 export default defineComponent({
-  name: "CustomizableWireframe",
-  setup() {
-    const breakpoint = useBreakpoint();
-    return { breakpoint, classNames };
-  },
+    name: "CustomizableWireframe",
+    components: { Wireframe }
 });
 </script>
