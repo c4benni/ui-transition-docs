bc311e17a0a9f2aa1c9c8ee26e121c586a7f1116
c4benni <c4benni@gmail.com>
2022-02-06 18:33:15 +0100

    create icons

---
+++ src/components/icons/Reset.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeWidth="2" d="M20 8c-1.403-2.96-4.463-5-8-5a9 9 0 1 0 0 18h0a9 9 0 0 0 9-9m0-9v6h-6"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'ResetIcon'
+})
+</script>
