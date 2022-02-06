6604dc6da3a7953cc3c4d0f1dd6358f43de93eea
c4benni <c4benni@gmail.com>
2022-02-06 18:33:34 +0100

    update LowLevelApi

--- src/components/sections/Main/LowLevelApi/index.vue
+++ src/components/sections/Main/LowLevelApi/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class="w-full relative fill-before isolate before:-z-1 before:bg-[hsl(0,0%,99%)] dark:before:bg-[hsl(180,5%,6%)] before:w-screen before:left-[50%] before:translate-x-[-50%] before:border-y before:border-gray-300/40 dark:before:border-gray-700/20 pt-6 pb-[72px] xs:pb-[108px] md:pb-[124px] sm:pt-12 xs:px-[0.375rem] sm:px-0"
+        class="w-full relative fill-before isolate before:-z-1 before:bg-[hsl(0,0%,98%)] dark:before:bg-[hsl(180,5%,7%)] before:w-screen before:left-[50%] before:translate-x-[-50%] before:border-y before:border-gray-300/40 dark:before:border-gray-700/20 pt-6 pb-[72px] xs:pb-[108px] md:pb-[124px] sm:pt-12 xs:px-[0.375rem] sm:px-0"
     >
         <FeatureIntro
             badge="Low level API"
