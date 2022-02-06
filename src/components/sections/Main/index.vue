8dad39d0737373525dc6ba0ec9a225f9b0040791
c4benni <c4benni@gmail.com>
2022-02-06 11:11:44 +0100

    update Main

--- src/components/sections/Main/index.vue
+++ src/components/sections/Main/index.vue
@@ -1,42 +1,40 @@
 <template>
-    <main
-        :class="[
-            'grid',
-            classNames.surfaceBG,
-            classNames.containerPadding
-        ]"
-    >
-        <div
-            :class="[
-                'mx-auto',
-                classNames.appMaxWidth,
-            ]"
-        >
-            <Intro/>
-            <Features/>
-            <Customizable/>
-            <Controllable/>
-            <GroupsAndDelays/>
-        </div>
-    </main>
+  <main :class="['grid', classNames.surfaceBG, classNames.containerPadding]">
+    <div :class="['mx-auto', classNames.appMaxWidth]">
+      <Intro />
+      <Features />
+      <Customizable />
+      <Controllable />
+      <GroupsAndDelays />
+      <LowLevelApi />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import classNames from '../../../utils/classNames'
-import Intro from './Intro/index.vue'
-import Features from './Features/index.vue';
-import Customizable from './Customizable/index.vue';
-import Controllable from './Controllable/index.vue';
-import GroupsAndDelays from './GroupsAndDelays/index.vue';
+import { defineComponent } from "vue";
+import classNames from "../../../utils/classNames";
+import Intro from "./Intro/index.vue";
+import Features from "./Features/index.vue";
+import Customizable from "./Customizable/index.vue";
+import Controllable from "./Controllable/index.vue";
+import GroupsAndDelays from "./GroupsAndDelays/index.vue";
+import LowLevelApi from "./LowLevelApi/index.vue";
 
 export default defineComponent({
-    name: "Main",
-    setup() {
-        return {
-            classNames
-        };
-    },
-    components: { Intro, Features, Customizable, Controllable, GroupsAndDelays }
-})
+  name: "Main",
+  components: {
+    Intro,
+    Features,
+    Customizable,
+    Controllable,
+    GroupsAndDelays,
+    LowLevelApi,
+  },
+  setup() {
+    return {
+      classNames,
+    };
+  },
+});
 </script>
