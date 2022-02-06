9df964a1c85a5932c8efff0fd3eb7a321fc382e3
c4benni <c4benni@gmail.com>
2022-02-06 11:11:22 +0100

    renamed GroupsAndDelays Advanced tab to Deferred

--- src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Deferred/Dialog.vue
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Deferred/Dialog.vue
@@ -1,35 +1,29 @@
 <template>
   <div
     :class="[
-      'absolute w-full h-full top-0 left-0 bg-white dark:bg-card-dark overflow-hidden md:pb-[42px]',
+      'absolute w-full h-full top-0 left-0 bg-black/80 overflow-hidden rounded-[inherit]',
       { invisible: !image },
     ]"
   >
-    <Img
-      v-if="image"
-      :public-id="image.src"
-      :alt="`Splash Image by ${image.owner}`"
-      quality="50"
-      class="h-[40%] md:h-[45%] w-full object-cover"
-    />
+    <figure class="block h-full w-full relative">
+      <div class="h-w-full">
+        <Img
+          v-if="image"
+          :public-id="image.src"
+          :alt="`Splash Image by ${image.owner}`"
+          quality="50"
+          class="h-full w-full object-cover"
+          loading-background="none"
+          decoding="auto"
+        />
+      </div>
 
-    <p class="font-bold text-[1.25em] px-[8px] mb-[8px] mt-[12px]">
-      {{ image?.owner }}
-    </p>
-
-    <template
-        v-for="i in 3"
-        :key="i"
-    >
-      <p class="px-[8px] text-[0.9em] text-paragraph dark:text-paragraph-dark">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae fuga
-        dignissimos dolor ex corrupti quam vitae, accusamus blanditiis, quo
-        expedita perferendis reiciendis ab necessitatibus adipisci sunt aperiam
-        sit non dolorum!
-      </p>
-
-      <br v-if="i !== 3"/>
-    </template>
+      <figcaption
+        class="text-[1.25em] px-[8px] mt-[24px] absolute bottom-0 pb-[24px] pt-[16px] bg-gradient-to-b from-black/5 via-black/70 to-black/90 w-full text-white"
+      >
+        By <strong>{{ image?.owner }}</strong>
+      </figcaption>
+    </figure>
 
     <Button
       icon
--- src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Deferred/index.vue
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/Deferred/index.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
-    <p
-      class="font-bold mx-[8px] mt-[24px] pb-[4px] mb-[24px] border-b text-[1.15em] text-headline dark:text-headline-dark border-gray-200 dark:border-gray-700"
+  <Wireframe>
+    <div
+      class="mx-[8px] mt-[16px] pb-[4px] mb-[24px] border-b border-gray-200 dark:border-gray-700"
     >
-      Click an image
-    </p>
+      <p class="font-bold text-[1.15em] text-headline dark:text-headline-dark">Unsplash images</p>
+
+      <p class="text-[0.85em] font-medium text-paragraph dark:text-paragraph-dark mt-[2px]">Click to expand</p>
+    </div>
 
     <div
       class="grid grid-rows-3 grid-cols-2 gap-x-[8px] gap-y-[12px] px-[8px] max-h-full"
     >
-      <picture
+      <figure
         v-for="(image, i) in splashImages"
         :key="image.id"
         :id="image.id"
-        class="cursor-pointer transition-opacity duration-75 active:opacity-70"
+        class="cursor-pointer transition-opacity duration-75 active:opacity-70 group"
         @click="selected = i"
       >
         <Img
           :public-id="image.src"
           :alt="`Splash Image by ${image.owner}`"
-          quality="50"
-          class="rounded h-[96px] w-full object-cover border border-gray-300/40 dark:border-gray-700/60"
+          quality="5"
+          height="96"
+          class="rounded h-[96px] w-full object-cover border border-gray-300/40 dark:border-gray-700/60 can-hover:group-hover:opacity-90 transition-opacity"
         />
 
         <figcaption
@@ -31,11 +34,11 @@
             {{ image.owner }}
           </strong>
         </figcaption>
-      </picture>
+      </figure>
     </div>
 
-    <Dialog/>
-  </div>
+    <Dialog />
+  </Wireframe>
 </template>
 
 <script lang="ts">
@@ -43,15 +46,15 @@ import { defineComponent } from "vue";
 import Img from "../../../../../../Img.vue";
 import { splashImages, selected } from "./state";
 import Dialog from "./Dialog.vue";
+import Wireframe from "../Wireframe/index.vue";
 
 export default defineComponent({
-  name: "Advanced",
-  components: { Img, Dialog },
+  name: "Deferred",
+  components: { Img, Dialog, Wireframe },
   setup() {
-
     return {
       splashImages,
-      selected
+      selected,
     };
   },
 });
