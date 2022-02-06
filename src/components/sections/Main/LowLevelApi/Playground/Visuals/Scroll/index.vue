02a02a07ad4c093664ce60cd27683c5667e7d608
c4benni <c4benni@gmail.com>
2022-02-06 15:22:22 +0100

    update LowLevelApi Scroll visuals

--- src/components/sections/Main/LowLevelApi/Playground/Visuals/Scroll/index.vue
+++ src/components/sections/Main/LowLevelApi/Playground/Visuals/Scroll/index.vue
@@ -1,73 +1,79 @@
 <template>
-  <Wireframe class="flex flex-col isolate">
-    <Generic showHeader>
-      <template #headerAction>
-        <Button
-          tag="div"
-          tabindex="-1"
-          class="w-[30px] translate-y-0 scale-100 px-0 h-[30px] rounded-full bg-white dark:bg-card-dark shadow-lg dark:shadow-none z-10 clip-none text-headline dark:text-headline-dark"
-          :title="`${state ? 'Close' : 'Open'} nav`"
-          @click="state = !state"
-        >
-          <IconWrapper>
-            <Component :is="state ? 'Close' : 'Menu'" />
-          </IconWrapper>
-        </Button>
-      </template>
-    </Generic>
-
-    <!-- nav menu -->
-    <ul
-      v-if="state"
-      class="bg-white dark:bg-black absolute w-h-full top-0 left-0 z-1 flex flex-col pt-[48px]"
+  <Wireframe class="flex flex-col isolate pb-[8px] md:pb-[32px]">
+    <p
+      class="mt-[24px] pb-[8px] border-b border-gray-200 dark:border-gray-700 mx-[12px] font-bold text-[1.25em]"
     >
+      Scroll
+    </p>
+
+    <div class="pt-[16px] grid gap-y-[16px] h-full max-h-full overflow-y-auto">
+      <div v-for="i in 4" :key="i">
+        <div
+          class="flex font-semibold justify-between items-center text-headline dark:text-headline-dark px-[12px] h-[32px]"
+        >
+          Section {{ i }}
+
+          <Button v-if="i !== 4" icon class="w-[32px] h-[32px] translate-y-0 scale-100">
+            <IconWrapper>
+              <JumpDown />
+            </IconWrapper>
+          </Button>
+        </div>
+
+        <div
+          class="text-paragraph dark:text-paragraph-dark mx-[12px] border-b pb-[16px] border-gray-200/70 dark:border-gray-700/70 border-dashed"
+        >
+          <p class="mb-[12px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            harum id nemo earum accusamus vel. Sed harum consequuntur cum fugit
+            deserunt fuga omnis nostrum similique, rerum assumenda voluptates
+            itaque culpa?
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            cumque doloremque corrupti voluptas, corporis eligendi aspernatur
+            fuga praesentium consequuntur commodi, molestias, unde maiores.
+            Magni, cum! Dolorum aliquam nulla molestias expedita!
+          </p>
+        </div>
+      </div>
+
       <Button
-        v-for="item in navItems"
-        :key="item"
-        tag="li"
+        text
+        tag="div"
         tabindex="-1"
-        :primary="false"
-        size="md"
-        class="translate-y-0 scale-100 text-[16px] w-full mt-[12px]"
-        @click="state = false"
+        size="sm"
+        class="w-full h-[32px] text-[14px]"
       >
-        {{ item }}
+        Scroll to top
+
+        <IconWrapper>
+          <ScrollTop/>
+        </IconWrapper>
       </Button>
-    </ul>
+    </div>
   </Wireframe>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
 import Wireframe from "../Wireframe/index.vue";
-import Button from "../../../../../../Inputs/Button.vue";
-import Menu from "../../../../../../icons/Menu.vue";
 import IconWrapper from "../../../../../../icons/IconWrapper.vue";
-import Close from "../../../../../../icons/Close.vue";
-import Generic from "../Wireframe/Generic.vue";
+import JumpDown from "../../../../../../icons/JumpDown.vue";
+import Button from "../../../../../../Inputs/Button.vue";
+import ScrollTop from "../../../../../../icons/ScrollTop.vue";
 
 export default defineComponent({
   name: "CustomizableRotateVisuals",
   components: {
     Wireframe,
-    Button,
-    Menu,
     IconWrapper,
-    Close,
-    Generic,
-  },
+    JumpDown,
+    Button,
+    ScrollTop
+},
 
-  setup() {
-    const navItems = Array(4)
-      .fill(true)
-      .map((_, i) => `Option ${i + 1}`);
-
-    const state = ref(false);
-
-    return {
-      navItems,
-      state,
-    };
-  },
+  setup() {},
 });
 </script>
