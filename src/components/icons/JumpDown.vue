9024b2d2185525268371f975dc634b69082eec55
c4benni <c4benni@gmail.com>
2022-02-06 15:18:46 +0100

    added icons

---
+++ src/components/icons/JumpDown.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 32 32"><path d="M13.414 17.586L18 22.172V8H8V6h10a2.002 2.002 0 0 1 2 2v14.172l4.586-4.586L26 19l-7 7l-7-7z" fill="currentColor"></path></svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'JumpDown'
+})
+</script>
