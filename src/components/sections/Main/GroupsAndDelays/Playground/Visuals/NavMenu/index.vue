86c689ef0bfe4573a129b082cf59057bbccff9aa
c4benni <c4benni@gmail.com>
2022-03-06 08:21:51 +0100

    feat: add UiTransition

--- src/components/sections/Main/GroupsAndDelays/Playground/Visuals/NavMenu/index.vue
+++ src/components/sections/Main/GroupsAndDelays/Playground/Visuals/NavMenu/index.vue
@@ -2,43 +2,52 @@
   <Wireframe class="flex flex-col isolate">
     <Generic showHeader>
       <template #headerAction>
-        <Button
-          tag="div"
-          tabindex="-1"
-          class="w-[30px] translate-y-0 scale-100 px-0 h-[30px] rounded-full bg-white dark:bg-card-dark shadow-lg dark:shadow-none z-10 clip-none text-headline dark:text-headline-dark"
-          :title="`${state ? 'Close' : 'Open'} nav`"
-          @click="state = !state"
-        >
-          <IconWrapper>
-            <Component :is="state ? 'Close' : 'Menu'" />
-          </IconWrapper>
-        </Button>
+        <UiTransition :duration="250">
+          <Button
+            :key="`${state}`"
+            tag="div"
+            tabindex="-1"
+            class="w-[30px] translate-y-0 scale-100 px-0 h-[30px] rounded-full bg-white dark:bg-card-dark shadow-lg dark:shadow-none z-10 clip-none text-headline dark:text-headline-dark"
+            :title="`${state ? 'Close' : 'Open'} nav`"
+            @click="state ? close() : (state = true)"
+          >
+            <IconWrapper>
+              <Component :is="state ? 'Close' : 'Menu'" />
+            </IconWrapper>
+          </Button>
+        </UiTransition>
       </template>
     </Generic>
 
     <!-- nav menu -->
-    <ul
-      v-if="state"
-      class="bg-white dark:bg-black absolute w-h-full top-0 left-0 z-1 flex flex-col pt-[48px]"
-    >
-      <Button
-        v-for="item in navItems"
-        :key="item"
-        tag="li"
-        tabindex="-1"
-        :primary="false"
-        size="md"
-        class="translate-y-0 scale-100 text-[16px] w-full mt-[12px]"
-        @click="state = false"
+    <UiTransition :delay="{ leave: 400 }">
+      <ul
+        v-if="state"
+        class="bg-white dark:bg-black absolute w-h-full top-0 left-0 z-1 flex flex-col pt-[48px]"
       >
-        {{ item }}
-      </Button>
-    </ul>
+        <UiTransition group :config="['slideY(-100, 0,`px`)', 'fade']">
+          <Button
+            v-if="showButtons"
+            v-for="(item, i) in navItems"
+            :key="item"
+            tag="li"
+            :data-uit-delay="`${i * 50}ms`"
+            tabindex="-1"
+            :primary="false"
+            size="md"
+            class="translate-y-0 scale-100 text-[16px] w-full mt-[12px]"
+            @click="close"
+          >
+            {{ item }}
+          </Button>
+        </UiTransition>
+      </ul>
+    </UiTransition>
   </Wireframe>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+import { nextTick, defineComponent, ref, watch } from "vue";
 import Wireframe from "../Wireframe/index.vue";
 import Button from "../../../../../../Inputs/Button.vue";
 import Menu from "../../../../../../icons/Menu.vue";
@@ -64,9 +73,26 @@ export default defineComponent({
 
     const state = ref(false);
 
+    const showButtons = ref(false);
+
+    watch(
+      () => state.value,
+      (newVal) => {
+        if (newVal) {
+          nextTick(() => (showButtons.value = state.value));
+        }
+      }
+    );
+
+    const close = () => {
+      (showButtons.value = false), nextTick(() => (state.value = false));
+    };
+
     return {
       navItems,
       state,
+      showButtons,
+      close,
     };
   },
 });
