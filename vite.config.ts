4697e54eb4b529d5ca61835b21f9f4b9e08b3469
c4benni <c4benni@gmail.com>
2022-03-14 22:50:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080,
-    host: true
-  }
-})
+    port: 2022,
+    host: true,
+  },
+});
