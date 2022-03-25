98121c9459cdfb86757aae65d9150e0d56a6fff9
c4benni <c4benni@gmail.com>
2022-03-25 08:16:14 +0100

    feat: add UiTransition

--- src/components/sections/Main/Controllable/Playground/Visuals/BottomSheet/TopDialog.vue
+++ src/components/sections/Main/Controllable/Playground/Visuals/BottomSheet/TopDialog.vue
@@ -1,82 +1,86 @@
 <template>
   <!-- top dialog -->
-  <div
-    v-if="bottomSheetState"
-    class="bg-white dark:bg-card-dark shadow-xl dark:shadow-md relative z-1 h-[108px]"
-  >
-    <!-- header -->
-    <div class="flex py-[8px] px-[14px]">
-      <IconWrapper
-        tag="button"
-        tabindex="-1"
-        title="Close"
-        class="text-lg justify-self-start w-[18px] h-[18px]"
-        @click="toggleBottomSheet"
-      >
-        <Close />
-      </IconWrapper>
-
-      <span class="font-semibold w-full flex justify-center">
-        Select destination
-      </span>
-    </div>
-
-    <!-- inputs -->
-
+  <UiTransition config="slideY(-100)" spring="stiff">
     <div
-      class="grid items-center gap-x-[8px] grid-flow-col grid-cols-[18px,1fr] px-[14px] pb-[8px]"
+      v-if="bottomSheetState"
+      class="bg-white dark:bg-card-dark shadow-xl dark:shadow-md relative z-1 h-[108px]"
     >
-      <!-- markers -->
-      <div
-        class="w-full h-[calc(100%-6px)] grid justify-center grid-rows-3 relative isolate"
-      >
-        <template v-for="(icon, i) in locationIcons" :key="icon">
-          <IconWrapper
-            :class="[
-              'z-1 relative',
-              {
-                'text-green-600 dark:text-green-500': i === 0,
-                'text-blue-600 dark:text-blue-500': i === 1,
-              },
-            ]"
-          >
-            <Component :is="icon" />
-          </IconWrapper>
+      <!-- header -->
+      <div class="flex py-[8px] px-[14px]">
+        <IconWrapper
+          tag="button"
+          tabindex="-1"
+          title="Close"
+          class="text-lg justify-self-start w-[18px] h-[18px]"
+          @click="toggleBottomSheet"
+        >
+          <Close />
+        </IconWrapper>
 
-          <div
-            v-if="i === 0"
-            class="h-[calc(100%+10px)] -mt-[5px] -z-1 relative w-[1.5px] bg-card justify-self-center bg-gradient-to-b from-green-600 dark:from-green-500 via-blue-600 dark:via-blue-500 to-blue-700 dark:to-blue-600"
-          />
-        </template>
+        <span class="font-semibold w-full flex justify-center">
+          Select destination
+        </span>
       </div>
 
-      <!-- text fields -->
-      <div>
+      <!-- inputs -->
+
+      <div
+        class="grid items-center gap-x-[8px] grid-flow-col grid-cols-[18px,1fr] px-[14px] pb-[8px]"
+      >
+        <!-- markers -->
         <div
-          class="h-[28px] flex items-center px-[8px] text-headline dark:text-headline-dark relative fill-before before:bg-current before:opacity-5 rounded w-[calc(100%-26px)] text-[13px]"
+          class="w-full h-[calc(100%-6px)] grid justify-center grid-rows-3 relative isolate"
         >
-          Current location
+          <template v-for="(icon, i) in locationIcons" :key="icon">
+            <IconWrapper
+              :class="[
+                'z-1 relative',
+                {
+                  'text-green-600 dark:text-green-500': i === 0,
+                  'text-blue-600 dark:text-blue-500': i === 1,
+                },
+              ]"
+            >
+              <Component :is="icon" />
+            </IconWrapper>
+
+            <div
+              v-if="i === 0"
+              class="h-[calc(100%+10px)] -mt-[5px] -z-1 relative w-[1.5px] bg-card justify-self-center bg-gradient-to-b from-green-600 dark:from-green-500 via-blue-600 dark:via-blue-500 to-blue-700 dark:to-blue-600"
+            />
+          </template>
         </div>
 
-        <div class="flex items-center w-full mt-[6px]">
+        <!-- text fields -->
+        <div>
           <div
-            class="flex-grow relative fill-before before:bg-current before:opacity-5 rounded h-[28px]"
+            class="h-[28px] flex items-center px-[8px] text-headline dark:text-headline-dark relative fill-before before:bg-current before:opacity-5 rounded w-[calc(100%-26px)] text-[13px]"
           >
-            <input
-              placeholder="Search destination"
-              v-model="searchDestination"
-              class="h-w-full rounded-[inherit] px-[8px] text-[13px] focus:text-base bg-[transparent] outline-none focus:border border-headline/20 dark:border-headline-dark/20 placeholder:text-[13px] py-[3px]"
-              tabindex="-1"
-            />
+            Current location
           </div>
 
-          <IconWrapper class="w-[18px] h-[18px] text-lg flex-shrink-0 ml-[8px]">
-            <Plus />
-          </IconWrapper>
+          <div class="flex items-center w-full mt-[6px]">
+            <div
+              class="flex-grow relative fill-before before:bg-current before:opacity-5 rounded h-[28px]"
+            >
+              <input
+                placeholder="Search destination"
+                v-model="searchDestination"
+                class="h-w-full rounded-[inherit] px-[8px] text-[13px] focus:text-base bg-[transparent] outline-none focus:border border-headline/20 dark:border-headline-dark/20 placeholder:text-[13px] py-[3px]"
+                tabindex="-1"
+              />
+            </div>
+
+            <IconWrapper
+              class="w-[18px] h-[18px] text-lg flex-shrink-0 ml-[8px]"
+            >
+              <Plus />
+            </IconWrapper>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </UiTransition>
 </template>
 
 <script lang="ts">
@@ -109,7 +113,7 @@ export default defineComponent({
       locationIcons,
       searchDestination,
       bottomSheetState,
-      toggleBottomSheet
+      toggleBottomSheet,
     };
   },
 });
